5366e7db07929fa5ba396a10280dbc0d8000faa2
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-19 19:56:31 +0100

    Update example to retrieve the logged in username

--- examples/python2.7--requests_oauthlib.py
+++ examples/python2.7--requests_oauthlib.py
@@ -3,12 +3,14 @@
 from requests_oauthlib import OAuth2Session
 
 
-client_id = 'your_client_id'
-client_secret = 'your_secret'
-redirect_uri = 'your_redirect_url'
+client_id = 'client_id'
+client_secret = 'client_secret'
+redirect_uri = 'http://redirect_uri/'
 
 authorize_url = 'https://kelder.zeus.ugent.be/oauth/oauth2/authorize/'
 access_token_url = 'https://kelder.zeus.ugent.be/oauth/oauth2/token/'
+
+account_url = 'https://kelder.zeus.ugent.be/oauth/api/current_user/'
 
 oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
 authorization_url, state = oauth.authorization_url(authorize_url)
@@ -20,5 +22,9 @@ authorization_response = raw_input('Enter the full callback URL: ')
 # Use verify=False as we don't have our verified certs yet
 token = oauth.fetch_token(access_token_url, authorization_response=authorization_response, client_secret=client_secret, verify=False)
 
+print 'Token obtained: %s' % token
 
-print token
+session = OAuth2Session(client_id, token=token, redirect_uri=redirect_uri)
+content = session.get(account_url, verify=False).content
+
+print content
