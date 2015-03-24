0617a93e038c8504cff3fc5c17bac76eb714ff8a
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-24 20:12:20 +0100

    Add static files configuration

--- oauth/oauth/urls.py
+++ oauth/oauth/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -16,4 +18,4 @@ urlpatterns = patterns('',
 
     # OAuth
     url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
