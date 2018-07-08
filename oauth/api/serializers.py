91a047755a66dc2cf0e029b1de606b94925dd297
Rien Maertens <maertensrien@gmail.com>
2018-07-09 01:00:09 +0200

    Try to add email to OAuth response

--- oauth/api/serializers.py
+++ oauth/api/serializers.py
@@ -2,5 +2,6 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=200)
+    username = serializers.CharField(max_length=255)
+    email = serializers.CharField(max_length=255)
     id = serializers.IntegerField()
